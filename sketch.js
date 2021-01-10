2e36d063fa70add3419441ba6ae8a121f1df08fe
mrunali399 <mrunali399@gmail.com>
2021-01-10 12:18:16 +0530

    Add base

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(150, 305, 200, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -58,7 +58,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+   
     bird.display();
     platform.display();
 }
